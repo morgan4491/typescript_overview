18ee74d9a5454a76f7cb7545b248648a810a2ae4
morgan4491 <m.morgan4491@gmail.com>
2024-10-04 14:40:10 -0400

    initial

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// let age: number = 50;
+
+// age = '50';   // if you try and reassign age to a string, and run index.ts after already declaring age as a number, it will give you an error
+
+// console.log(age);
+
+
+
+function getUserInput () {
+    const userName = prompt('Please type your name');
+    const age: any = prompt('Please enter your age');
+
+    const userData = {
+        userName: userName,
+        age: age
+    };
+
+    return userData;
+}
