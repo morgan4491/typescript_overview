8cb46d0d59fd22de96d724f664847b6c40152c4d
morgan4491 <m.morgan4491@gmail.com>
2024-10-07 14:37:21 -0400

    worked on vite note app

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const noteForm = document.querySelector('#note-form');
+
+function addNote(event: Event) {
+  event?.preventDefault();
+
+  console.log('submit')
+
+}
+
+noteForm?.addEventListener('click', addNote);
